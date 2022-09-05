86735bfe596d7932d574397ab2c4cfd2be03d259
David Malicke <davemalicke@gmail.com>
2022-09-05 09:03:36 -0400

    Initial Commit

---
+++ Study-URLSession/Study_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_URLSessionApp.swift
+//  Study-URLSession
+//
+//  Created by David Malicke on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Study_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
